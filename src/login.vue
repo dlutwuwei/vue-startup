b7f1664361f6b998d196ba8cb01ceebbd52a6e10
wuwei <wuwei@bytedance.com>
2017-11-24 23:48:46 +0800

    first commit

---
+++ src/login.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container">
+        <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="账号" prop="account">
+                <el-input type="text" v-model="ruleForm2.account" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="pass">
+                <el-input type="password" v-model="ruleForm2.password" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submitForm('ruleForm2')">登陆</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            var validatePass = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('请输入密码'));
+                } else {
+                    if (this.ruleForm2.checkPass !== '') {
+                        this.$refs.ruleForm2.validateField('checkPass');
+                    }
+                    callback();
+                }
+            };
+            var validateAccount = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('请输入账号'));
+                } else {
+                    callback();
+                }
+            };
+            return {
+                ruleForm2: {
+                    pass: '',
+                    account: ''
+                },
+                rules2: {
+                    account: [
+                        { validator: validateAccount, trigger: 'blur' }
+                    ],
+                    pass: [
+                        { validator: validatePass, trigger: 'blur' }
+                    ]
+                }
+            };
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            }
+        }
+    }
+</script>
+<style lang="scss">
+    @import '../scss/login.scss';
+</style>
